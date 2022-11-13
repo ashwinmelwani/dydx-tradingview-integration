7270245cb303b58c587fce67e74f57c473b927f9
ashwinmelwani <amelwani9@gmail.com>
2022-11-13 01:01:31 +0000

    Create getPositions.ts

---
+++ src/services/getPositions.ts
@@ -0,0 +1,20 @@
+import DYDXConnector from './client';
+import { PositionResponseObject } from '@dydxprotocol/v3-client';
+import config = require('config');
+import 'dotenv/config';
+
+export const getPositions = async () => {
+	try {
+		const connector = await DYDXConnector.build();
+		const positions: { positions: PositionResponseObject[] } = await client.private.getPositions(
+  {
+    market: Market.ETH_USD,
+    status: PositionStatus.OPEN,
+  },
+);
+		
+		return positions;
+	} catch (error) {
+		console.error(error);
+	}
+};
