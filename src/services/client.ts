881dad1e90eaade581463be97a9f5b5d104cdbde
ashwinmelwani <amelwani9@gmail.com>
2022-11-13 22:17:40 +0000

    added connector.client

--- src/services/client.ts
+++ src/services/client.ts
@@ -47,7 +47,7 @@ class DYDXConnector {
 			await connector.client.private.getAccount(process.env.ETH_ADDRESS);
 		const markets: { markets: MarketsResponseObject } = 
 		      	await connector.client.public.getMarkets();
-		const positions: { positions: PositionResponseObject[] } = await client.private.getPositions(
+		const positions: { positions: PositionResponseObject[] } = await connector.client.private.getPositions(
   {
     market: Market.ETH_USD,
     status: PositionStatus.OPEN,
